3959fe5199c05ecc30069d8a10998f7d16ed9f83
Anatoly <anatolypanushkin@mail.ru>
2022-07-19 14:22:13 +0300

    Initial commit

---
+++ PersonalAccount/Data/AirCompaniesContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PersonalAccount.Data
+{
+    public partial class AirCompaniesContext : DbContext
+    {
+        public AirCompaniesContext()
+        {
+        }
+
+        public AirCompaniesContext(DbContextOptions<AirCompaniesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AirlineCompany> AirlineCompanies { get; set; } = null!;
+        public virtual DbSet<DataAll> DataAlls { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSnakeCaseNamingConvention();
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=AirCompanies;Username=postgres;Password=1234");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DataAll>(entity =>
+            {
+                entity.HasNoKey();
+            });
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PersonalAccount/Services/PersonalAccService.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using PersonalAccount.Data;
+using PersonalAccount.Mapper;
+
+namespace PersonalAccount.Services;
+
+public class PersonalAccService:IPersonalAccService
+{
+    private readonly AirCompaniesContext _context;
+
+    public PersonalAccService(AirCompaniesContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<ICollection<DataAll>> ByDocNumberAsync(ByDocNumber byDocNumber)
+    {
+        StreamReader stream = new StreamReader("SqlRows\\ByDocNumberSql.txt"); 
+        
+        string? buffer = await stream.ReadToEndAsync();
+        
+        string? parameterizedSqlRow = string.Format(buffer,byDocNumber.DocNumber) ;
+        
+        var result = await _context.DataAlls.FromSqlRaw(parameterizedSqlRow).ToListAsync();
+        return result;
+    }
+
+    public async Task<ICollection<DataAll>> ByTicketNumberAsync(ByTicketNumber byTicketNumber)
+    {
+        string request = "";
+        if (byTicketNumber.CheckBox)
+        {
+            StreamReader stream = new StreamReader("SqlRows\\ByTicketNumberAllTicketSql.txt");
+            string? buffer = await stream.ReadToEndAsync();
+            request = string.Format(buffer,byTicketNumber.TicketNumber) ;
+        }
+        else
+        {
+            StreamReader stream = new StreamReader("SqlRows\\ByTicketNumberSelectedTicketSql.txt");
+            string? buffer = await stream.ReadToEndAsync();
+            request = string.Format(buffer, byTicketNumber.TicketNumber) ; 
+        }
+        
+        var result = await _context.DataAlls.FromSqlRaw(request).ToListAsync();
+        return result;
+    }
+
+    public async Task<ICollection<PrintXlsx>> ByDocNumberPrintAsync(ByDocNumberPrint byDocNumberPrint)
+    {
+        StreamReader stream = new StreamReader("SqlRows\\ByDocNumberPrintSql.txt");
+        string? buffer = await stream.ReadToEndAsync();
+        string? parameterizedSqlRow = string.Format(buffer,byDocNumberPrint.AirlineCompanyIataCode, byDocNumberPrint.DocNumber) ;
+        var result = await _context.DataAlls.FromSqlRaw(parameterizedSqlRow).ToListAsync();
+        return result.MapToFromatForPrint();
+    }
+
+    public async Task<ICollection<PrintXlsx>> ByTicketNumberPrintAsync(ByTicketNumberPrint byTicketNumberPrint)
+    {
+        string request = "";
+        if (byTicketNumberPrint.ByTicketNumberCheckBox)
+        {
+            StreamReader stream = new StreamReader("SqlRows\\ByTicketNumberPrintAllTicketSql.txt");
+            string? buffer = await stream.ReadToEndAsync();
+            request = string.Format(buffer,byTicketNumberPrint.AirlineCompanyIataCode, byTicketNumberPrint.TicketNumber) ;
+            
+        }
+        else
+        {
+            StreamReader stream = new StreamReader("SqlRows\\ByTicketNumberPrintSelectedTicketSql.txt");
+            string? buffer = await stream.ReadToEndAsync();
+            request = string.Format(buffer,byTicketNumberPrint.AirlineCompanyIataCode, byTicketNumberPrint.TicketNumber) ;
+        }
+        
+        
+        var result = await _context.DataAlls.FromSqlRaw(request).ToListAsync();
+        return result.MapToFromatForPrint();
+    }
+
+    public async Task<ICollection<DataAll>> XlsxByTicketNumber(ByTicketNumberPrint byTicketNumberPrint)
+    {
+        string request = "";
+        if (byTicketNumberPrint.ByTicketNumberCheckBox)
+        {
+            StreamReader stream = new StreamReader("SqlRows\\ByTicketNumberPrintAllTicketSql.txt");
+            string? buffer = await stream.ReadToEndAsync();
+            request = string.Format(buffer,byTicketNumberPrint.AirlineCompanyIataCode, byTicketNumberPrint.TicketNumber) ;
+            
+        }
+        else
+        {
+            StreamReader stream = new StreamReader("SqlRows\\ByTicketNumberPrintSelectedTicketSql.txt");
+            string? buffer = await stream.ReadToEndAsync();
+            request = string.Format(buffer,byTicketNumberPrint.AirlineCompanyIataCode, byTicketNumberPrint.TicketNumber) ;
+        }
+         
+        var result = await _context.DataAlls.FromSqlRaw(request).ToListAsync();
+        return result;
+        
+    }
+
+
+    
+}
